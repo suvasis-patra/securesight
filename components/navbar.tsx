fd94ae87ba9777466a63dc7937549b3fc11a99d6
suvasis-patra <suvasis02@kgpian.iitkgp.ac.in>
2025-07-22 08:01:12 +0530

    add navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,40 @@
-import React from "react";
-
+import { navbarItems } from "@/utils/constants";
+import { ChevronDown } from "lucide-react";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="flex justify-between px-10 py-4">
+      <div className="flex items-center gap-3">
+        <img src="./image/logo.svg" alt="logo" />
+        <span className="text-2xl font-semibold">SecureSight</span>
+      </div>
+      <div className="flex items-center gap-6">
+        {navbarItems.map((item) => (
+          <li key={item.label} className="flex items-center gap-2">
+            <img src={item.iconLink} alt={item.label} width={30} />
+            <span className="capitalize font-semibold text-base">
+              {item.label}
+            </span>
+          </li>
+        ))}
+      </div>
+      <div className="flex items-center gap-2">
+        <div className="flex items-center justify-center h-[40px] w-[40px] rounded-full overflow-hidden bg-gray-200">
+          <img
+            src="/image/avatar.png"
+            alt="avatar"
+            className="h-full w-full object-cover"
+          />
+        </div>
+        <div className="flex flex-col">
+          <span className="font-semibold">Mohammed Ajhas</span>
+          <span className="mt-[-5px]">ajhas@securesigth.com</span>
+        </div>
+        <span>
+          <ChevronDown />
+        </span>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
---
+++ utils/constants.ts
@@ -0,0 +1,22 @@
+export const navbarItems = [
+  {
+    label: "dashboard",
+    iconLink: "./image/dashboard.svg",
+  },
+  {
+    label: "camera",
+    iconLink: "./image/camera.svg",
+  },
+  {
+    label: "scenes",
+    iconLink: "./image/scenes.svg",
+  },
+  {
+    label: "incidents",
+    iconLink: "./image/incident.svg",
+  },
+  {
+    label: "users",
+    iconLink: "./image/user.svg",
+  },
+] as const;
